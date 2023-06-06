2f2998e38ea23b92714c3036cc864f687f8755a5
rbharath <87760148+rbharath89@users.noreply.github.com>
2023-06-06 09:56:58 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,7 +18,7 @@ public class App {
     public void init()
     {
         Logger log = LoggerFactory.getLogger(App.class);
-        log.info("Java app started at begining");
+        log.info("Java app started");
     }
 
     public String getStatus() {
